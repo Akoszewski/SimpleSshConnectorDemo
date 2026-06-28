288c9e986ac9008e4c0571dba591dd26e4339d7b
Arkadiusz Koszewski <akoszews@gmail.com>
2026-06-28 17:50:59 +0000

    Add build script

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+export ANDROID_HOME=/home/debian/Android/Sdk
+export ANDROID_SDK_ROOT=/home/debian/Android/Sdk
+export JAVA_HOME=/opt/android-studio/jbr
+export PATH=/home/debian/Android/Sdk/platform-tools:/home/debian/Android/Sdk/cmdline-tools/latest/bin:/home/debian/.local/bin:/usr/local/bin:/usr/bin:/bin
+
+gradle assembleDebug
